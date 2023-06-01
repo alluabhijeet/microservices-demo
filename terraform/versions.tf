85b20f92a6c792f667124a7c8078414de3ec0f7e
Abhijeet Allu <abhijeet.allu.work@gmail.com>
2023-06-01 11:06:29 -0500

    adding kubectl provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "4.67.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 
   required_version = ">= 1.0"
